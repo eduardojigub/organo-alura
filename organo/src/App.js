84939d68d0cd88e7bba8da1d3b3e4269b43b849c
eduardojigub <eduardobrunoit@gmail.com>
2022-08-18 21:21:29 -0300

    creating Input Text component

--- organo/src/App.js
+++ organo/src/App.js
@@ -1,9 +1,13 @@
-import Banner from './componentes/Banner/Banner'
+import Banner from "./componentes/Banner/Banner";
+import TextInput from "./componentes/InputText/TextInput";
 
 function App() {
   return (
     <div>
       <Banner />
+      <TextInput label="Nome" placeholder="Digite seu nome" />
+      <TextInput label="Cargo" placeholder="Digite seu cargo" />
+      <TextInput label="Imagem" placeholder="Digite o endereço da imagem" />
     </div>
   );
 }
