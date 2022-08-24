41f2761bb6f889790d5e838104658c0ded73941d
eduardojigub <eduardobrunoit@gmail.com>
2022-08-24 15:51:10 -0300

    adding props on Colaborador component

--- organo/src/componentes/Colaborador/Colaborador.jsx
+++ organo/src/componentes/Colaborador/Colaborador.jsx
@@ -1,14 +1,14 @@
 import "./Colaborador.css";
 
-const Colaborador = () => {
+const Colaborador = ({ nome, cargo, imagem }) => {
   return (
     <div className="colaborador">
       <div className="cabecalho">
-        <img src="https://github.com/eduardojigub.png" alt="Foto Eduardo" />
+        <img src={imagem} alt="Foto Pessoa" />
       </div>
       <div className="rodape">
-        <h4>Eduardo</h4>
-        <h5>Desenvolvedor</h5>
+        <h4>{nome}</h4>
+        <h5>{cargo}</h5>
       </div>
     </div>
   );
--- organo/src/componentes/Time/Time.jsx
+++ organo/src/componentes/Time/Time.jsx
@@ -5,7 +5,9 @@ const Time = (props) => {
   return (
     <section className="time" style={{ backgroundColor: props.corSecundaria }}>
       <h3 style={{ borderColor: props.corPrimaria }}>{props.nome}</h3>
-      <Colaborador />
+      {props.users.map((user) => (
+        <Colaborador nome={user.nome} cargo={user.cargo} imagem={user.imagem} />
+      ))}
     </section>
   );
 };
