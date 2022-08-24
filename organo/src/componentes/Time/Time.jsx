ed40d707cc5aee3c2bcacfc0c0881caccb8c73af
eduardojigub <eduardobrunoit@gmail.com>
2022-08-24 16:12:04 -0300

    adding ternarium to show only arrays bigger than 1

--- organo/src/componentes/Time/Time.jsx
+++ organo/src/componentes/Time/Time.jsx
@@ -3,18 +3,23 @@ import "./Time.css";
 
 const Time = (props) => {
   return (
-    <section className="time" style={{ backgroundColor: props.corSecundaria }}>
-      <h3 style={{ borderColor: props.corPrimaria }}>{props.nome}</h3>
-      <div className="colaboradores">
-        {props.users.map((user) => (
-          <Colaborador
-            nome={user.nome}
-            cargo={user.cargo}
-            imagem={user.imagem}
-          />
-        ))}
-      </div>
-    </section>
+    props.users.length > 0 && (
+      <section
+        className="time"
+        style={{ backgroundColor: props.corSecundaria }}
+      >
+        <h3 style={{ borderColor: props.corPrimaria }}>{props.nome}</h3>
+        <div className="colaboradores">
+          {props.users.map((user) => (
+            <Colaborador
+              nome={user.nome}
+              cargo={user.cargo}
+              imagem={user.imagem}
+            />
+          ))}
+        </div>
+      </section>
+    )
   );
 };
 
